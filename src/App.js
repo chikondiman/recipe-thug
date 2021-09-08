95d4cb317c4db85d75a81d1da75f1dfbc43d438f
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-08 08:08:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ const App = () => {
 
   const APP_ID = "57586b46";
   const APP_KEY = "d873aa7435270f0814995651f04dce76";
-  const exampleREQ = "https://api.edamam.com/api/recipes/v2"
+  const exampleREQ = "https://api.edamam.com/api/recipes/v2";
   return (
 <div className="App">
 <h1>Waddup from Recipe-Thug</h1>
+<p>home of the food that makes you wanna sock ya mama</p>
 </div>
   );
 }
