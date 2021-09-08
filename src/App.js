526f3db5a68c7cdd8800dab4726ba2caa8c07106
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-08 08:03:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 
 const App = () => {
+
+  const APP_ID = "57586b46";
+  const APP_KEY = "d873aa7435270f0814995651f04dce76";
   return (
 <div className="App">
 <h1>Waddup from Recipe-Thug</h1>
